0f46ea171d8adb63f9b7924daca9c2fc887b9729
Victor Bertolini <118063703+Bertolini-Victor@users.noreply.github.com>
2025-11-27 14:38:58 -0300

    create the routes of the page

---
+++ src/RoutesConfig.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home.jsx'
+import About from './pages/about/about.jsx'
+import Game from './pages/game/game.jsx'
+
+const RoutesConfig = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/game/:difficulty" element={<Game />} />
+        </Routes>
+    );
+}
